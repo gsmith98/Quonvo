139cac8930ec7c131f2ca87ce3733e3a0b9dc9f5
gsmith98 <smitgw11@gmail.com>
2017-03-13 17:57:43 -0700

    put in a test comment

--- quonvoApp/app.js
+++ quonvoApp/app.js
@@ -2,6 +2,7 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('../css/styles.css');
 
+// a test change
 ReactDOM.render(
   <a>Worked!</a>,
   document.getElementById('root')

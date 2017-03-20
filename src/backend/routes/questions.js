e9b578a7f000888e0c34228b1aaf2b1c5849caed
Mitchell Troyanovsky <mtroyanovsky1@babson.edu>
2017-03-20 14:25:42 -0700

    Created the ability to add a question

--- src/backend/routes/questions.js
+++ src/backend/routes/questions.js
@@ -9,11 +9,10 @@ console.log('I got to questions')
 // content. The user will be taken from req.user which exists because of the
 // session. Another thing to note is that the rest of the question object defaults
 // to the values we put in our model.
-router.post('questions/new', (req, res) => {
-  const user = 123
+router.post('/questions/new', (req, res) => {
   const question = new Question({
-    label: req.body.label,
-    author: user,
+    subject: req.body.label,
+    asker: req.user.id,
     content: req.body.content
   });
   question.save()

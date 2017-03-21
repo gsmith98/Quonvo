cdc031dfb1f99f769fba11b75dfff78d04ec12d2
Holly <hrli@wharton.upenn.edu>
2017-03-21 10:59:22 -0700

    bigQ

---
+++ src/frontend/components/BigQ.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const BigQ = ({ onPressButton }) => (
+
+  <div className="display_container">
+    <span className="display"> Q</span>
+    <div className="header_block">
+      <div className="spacer" />
+      <div className="header"> Ask me anything. </div>
+      <div className="explanation">
+      Quonvo lets real humans answer your questions.
+      After all, we learn our best life lessons from people, not Google searches. </div>
+      <div className="button_ask_container">
+        <button className="ask_button" onClick={onPressButton}> GET AN ANSWER </button>
+      </div>
+    </div>
+
+  </div>
+
+
+  );
+
+export default BigQ;

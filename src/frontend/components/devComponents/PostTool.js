78b8c85ea17eea62387a285aec0dca165698f65a
gsmith98 <smitgw11@gmail.com>
2017-03-25 00:42:08 -0700

    adjusted tool so posts can be made to different endpoints

--- src/frontend/components/devComponents/PostTool.js
+++ src/frontend/components/devComponents/PostTool.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 import ObjectBuilder from './ObjectBuilder';
 
 class PostTool extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { target: props.url };
+  }
+
   postButtonClick(data) {
-    console.log('Posting data:', data);
-    fetch(this.props.url, {
+    console.log('Posting data to:', this.state.target, data);
+    fetch(this.state.target, {
       method: 'post',
       body: JSON.stringify(data)
     }).then((resp) => {
@@ -15,10 +20,21 @@ class PostTool extends Component {
     });
   }
 
+  handleChange(event) {
+    this.setState({ target: event.target.value });
+  }
+
   render() {
     return (
       <div>
         <p>POST Tool</p>
+        Post to:{'              '}
+        <input
+          type="text"
+          onChange={event => this.handleChange(event)}
+          value={this.state.target}
+          style={{ width: 400 }}
+        />
         <ObjectBuilder
           doWithObj={(data) => { this.postButtonClick(data); }}
           buttonMsg="POST"

39ba9c1df7c0ac66e6f6463eb011cff53e26a521
gsmith98 <smitgw11@gmail.com>
2017-03-28 16:00:39 -0700

    some lovely es6 formatting

--- src/backend/routes/auth.js
+++ src/backend/routes/auth.js
@@ -7,20 +7,14 @@ const User = models.User;
 module.exports = (passport) => {
   // You will use passport to authenticate in the future
   router.post('/local/signup', (req, res) => {
-    const user = new User({
+    new User({
       email: req.body.email.toLowerCase(),
       password: req.body.password,
       interests: req.body.interests
-    });
-    user.save()
-    .then(newUser => res.json({
-      success: true,
-      user: newUser
-    }))
-    .catch(err => res.json({
-      success: false,
-      error: err
-    }));
+    })
+    .save()
+    .then(user => res.json({ success: true, user }))
+    .catch(error => res.json({ success: false, error }));
   });
 
   // TODO is there good reason for this custom authenticate handler?

95a2fb49489f8be74e93d81b32ddb0448c76a0f9
gsmith98 <smitgw11@gmail.com>
2017-03-29 16:53:39 -0700

    socket connections are given access to user object upon connection

--- src/backend/socketConfig.js
+++ src/backend/socketConfig.js
@@ -1,17 +1,26 @@
 const cookie = require('cookie');
+const models = require('./models');
 
-const socketHandler = mongoStore => (socket) => {
-  socket.emit('heyo', { data: 'mayo' });
+const socketHandler = mongoStore => (connection) => {
+  const socket = connection;
 
-  console.log('1:', socket.handshake.headers.cookie);
+  // Get user out of cookie and attach to this socket
+  if (!socket.handshake.headers.cookie) {
+    console.log('Socket connection attempted without user cookie');
+    return;
+  }
   const tS = cookie.parse(socket.handshake.headers.cookie)['connect.sid'];
-  console.log('2:', tS);
   const sessionID = tS.split('.')[0].split(':')[1];
-  console.log('3:', sessionID);
-  mongoStore.get(sessionID, (err, sesh) => {
-    console.log('4:', sesh);
-    console.log('5:', sesh.passport);
-  });
+  mongoStore.get(sessionID)
+  .then(sesh => models.User.findById(sesh.passport.user))
+  .then((user) => { socket.user = user; })
+  .catch(error => console.log(error));
+
+  // past this point, socket.user can be used in each socket.on
+  // TODO right now, the user object is grabbed at socket connection time.
+  //    if it is changed in mongo while the connection is open the changes
+  //    may not be reflected. Rather than grab the user object on connection,
+  //    should we grab it anew within each .on? This is what passport does
 };
 
 module.exports = socketHandler;

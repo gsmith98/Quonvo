5b98d3ae1d85b3791a90bd36099b888392516c85
Mitchell Troyanovsky <mtroyanovsky1@babson.edu>
2017-04-04 10:37:15 -0700

    SideBar presentational

---
+++ src/frontend/components/presentationalComponents/Archives.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Archives = ({ onPressButton }) => (
+  <div onClick={onPressButton} className="menu_icon">
+    <img alt="" src="./assets/archive_icon-01.png" className="icon" />
+    <span className="menu_bold"> Check out some archived answers </span>
+  </div>
+
+);
+
+export default Archives;
--- src/frontend/components/presentationalComponents/index.js
+++ src/frontend/components/presentationalComponents/index.js
@@ -1,3 +1,8 @@
 export { default as BigQ } from './BigQ';
 export { default as SigninBar } from './SigninBar';
 export { default as WriteQuestion } from './WriteQuestion';
+export { default as Archives } from './Archives';
+export { default as AskAQuestion } from './AskAQuestion';
+export { default as Profile } from './Profile';
+export { default as YourQuestions } from './YourQuestions';
+export { default as SideBar } from './SideBar';

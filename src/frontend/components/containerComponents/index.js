22d519271592d3c8cc75e8e43185fef41942ff3a
gsmith98 <smitgw11@gmail.com>
2017-04-03 17:32:29 -0700

    think it worked

--- src/frontend/components/containerComponents/index.js
+++ src/frontend/components/containerComponents/index.js
@@ -1,5 +1,4 @@
 export { default as SigninBarContainer } from './SigninBarContainer';
-import blue from 'blue';
 
 // the export below is strictly in place for linting reasons.
 // TODO remove this the moment a second container component is added to this file

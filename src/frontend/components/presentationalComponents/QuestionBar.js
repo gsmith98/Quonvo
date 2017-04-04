876ca0615c5341840705f3b1f7c9fe8fba2fe76e
Mitchell Troyanovsky <mtroyanovsky1@babson.edu>
2017-04-04 14:35:18 -0700

    Question components

--- src/frontend/components/presentationalComponents/QuestionBar.js
+++ src/frontend/components/presentationalComponents/QuestionBar.js
@@ -3,7 +3,6 @@ import LiveQuestion from './LiveQuestion';
 // The liftOfQustions is an array of objects that has the keys content, subject, and id
 const QuestionBar = (listOfQuestions, onQuestionClick) => (
   <div className="question_bar">
-  <p> Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii </p>
     <div
       className="question_header bold"
       style={{ fontSize: '18px', padding: '2vh 0vh 0vh 3vh' }}

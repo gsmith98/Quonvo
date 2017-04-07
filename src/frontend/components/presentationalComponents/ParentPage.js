1d0a8f681b5504489f60bbf8e96928fc5215ef26
Holly <hrli@wharton.upenn.edu>
2017-04-07 15:04:00 -0700

    got the chat part and lend an ear display to work together

--- src/frontend/components/presentationalComponents/ParentPage.js
+++ src/frontend/components/presentationalComponents/ParentPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { ChatContainer, QuestionBarContainer } from '../containerComponents';
-// import { SideBar } from './SideBar';
+import SideBar from './SideBar';
 
 const ParentPage = ({ chatopen }) => (
   <div className="page_container">
     <QuestionBarContainer />
     {chatopen ? <ChatContainer /> : null}
-
+    <SideBar />
   </div>
 );
 

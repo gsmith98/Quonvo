86944aa4b22f9b4c42da6f78febdd6b966a50572
Mitchell Troyanovsky <mtroyanovsky1@babson.edu>
2017-04-11 21:02:18 -0700

    added a couple thunks

--- src/frontend/actions/index.js
+++ src/frontend/actions/index.js
@@ -1,4 +1,8 @@
-import { signIn as apiSignIn } from 'api';
+import {
+   signIn as apiSignIn,
+   sendMessage as apiSendMessage,
+   createQuestion as apiCreateQuestion
+   } from 'api';
 // thunk
 export const signIn = (email, password) => (/* dispatch */) => {
   apiSignIn(email, password)
@@ -16,26 +20,47 @@ export const signIn = (email, password) => (/* dispatch */) => {
 
 // TODO make a thunk that hits API and uses id from response
 // or make it take an id and let whoever calls it do the saving
-let nextid = 0;
-const newMessage = (content, user) => ({
+
+const newMessage = (content, id, user) => ({
   type: 'NEW_MESSAGE',
   message: {
     content,
     user,
-    id: nextid++
+    id
   }
 });
 
-export const sendMessage = content => newMessage(content, 'YOU');
-export const receiveMessage = content => newMessage(content, 'THEM');
+const newMessageThunk = (chatId, content, user) => (dispatch) => {
+  apiSendMessage(chatId, content)
+  .then((responseJson) => {
+    const id = responseJson.message.id;
+    dispatch(newMessage(content, id, user));
+  })
+  .catch((err) => {
+    console.log('error');
+    throw err;
+  });
+};
+
+
+export const sendMessage = (content, chatId) => newMessageThunk(chatId, content, 'YOU');
+export const receiveMessage = (content, chatId) => newMessageThunk(chatId, content, 'THEM');
 
 export const openChat = () => ({ type: 'OPEN_CHAT' });
 export const closeChat = () => ({ type: 'CLOSE_CHAT' });
 export const newChattingPatner = partner => ({ type: 'NEW_PARTNER', partner });
 export const joinRoom = room => ({ type: 'JOIN_ROOM', room });
 
-export const onQuestionClick = questionId => (dispatch) => {
-  dispatch(joinRoom(questionId));
-  dispatch(newChattingPatner(questionId)); // TODO get actual chattingPartner handle
-  dispatch(openChat());
+export const onQuestionClick = (questionId, label, content) => (dispatch) => {
+  apiCreateQuestion(label, content)
+  .then((responseJson) => {
+    console.log('responseJson', responseJson);
+    dispatch(joinRoom(questionId));
+    dispatch(newChattingPatner(questionId)); // TODO get actual chattingPartner handle
+    dispatch(openChat());
+  })
+  .catch((err) => {
+    console.log('error');
+    throw err;
+  });
 };

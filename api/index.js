de35c0df65a3de86eba400fc47e77e8f675ad501
Mitchell Troyanovsky <mtroyanovsky1@babson.edu>
2017-05-02 11:18:57 -0700

    heroku sucs

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 
 const urlRoot = DOMAIN;
-console.log('urlroothi', urlRoot);
+console.log('urlroot', urlRoot);
 
 // TODO remove console logs (or log only in dev env)
 const post = (url, data) => {

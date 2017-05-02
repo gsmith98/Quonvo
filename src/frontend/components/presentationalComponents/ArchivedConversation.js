bc7ffecfefc1f201a54f559c2ae294d79a61d660
Mtroyanovsky1 <mtroyanovsky1@babson.edu>
2017-05-02 09:37:47 -0700

    Update ArchivedConversation.js

--- src/frontend/components/presentationalComponents/ArchivedConversation.js
+++ src/frontend/components/presentationalComponents/ArchivedConversation.js
@@ -1,13 +1,13 @@
 import React from 'react';
 // Change the class names to their own class names later on
 const ArchivedConversation = ({ messages, backToArchives }) => (
-    <div className="chat_body">
-      <div onClick={() => backToArchives()}>GO BACK</div>
-      {messages.map(message =>
-        <div key={message.id} className={`chatmessage${message.user}`}>
-          <div className="message_text"> {message.content} </div>
-        </div>
-      )}
-    </div>
+  <div className="chat_body">
+    <div onClick={() => backToArchives()}>GO BACK</div>
+    {messages.map(message =>
+      <div key={message.id} className={`chatmessage${message.user}`}>
+        <div className="message_text"> {message.content} </div>
+      </div>
+    )}
+  </div>
 );
 export default ArchivedConversation;
